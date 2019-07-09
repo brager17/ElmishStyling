2216045ef915c049229c9075d20ffc7907a7ce02
Евгений <eter55@yandex.ru>
2019-07-09 06:34:17 +0300

    updgrade

--- ElmishStyle/LibraryElmish.fs
+++ ElmishStyle/LibraryElmish.fs
@@ -5,6 +5,15 @@ open System
 open System
 open System.Drawing
 open Seed
+module oneSomeWorkflow =
+    type OneSomeWorkflow() =
+        member this.Bind(x, f) =
+            match x with
+            | Some s -> s
+            | None -> f x
+
+        member this.Return(x) = x
+    let onesome = OneSomeWorkflow();
 
 type SnapshotInfo =
     {
@@ -16,12 +25,16 @@ type SnapshotInfo =
 
 let mutable (storage: SnapshotInfo list) = [];
 
+type ChangePosition =
+    | Up
+    | Down
+
 type TapKey =
     | Button of ConsoleKey
 
 type CountStatesBack = CountStatesBack of int
 
-type RevertModel = (CountStatesBack  * SnapshotInfo)
+type RevertModel = CountStatesBack * SnapshotInfo
 
 type ReverseMsg =
     | KeyButton of ConsoleKey
@@ -40,36 +53,8 @@ type Msg =
     | ReverseMsg of ReverseMsg
 
 let init() =
-    { text = ""; yPosition = 0; color = ConsoleColor.Black; formatText = "" }, Cmd.ofMsg ((Author.Block, ConsoleColor.Black) |> ChangeAuthorAndColor |> EnterText)
+    { text = ""; yPosition = 0; color = ConsoleColor.Black; formatText = "" }, Cmd.ofMsg ((Author.Block, ConsoleColor.Black))
 
-let view (model: Model) dispatch =
-
-
-            Console.Clear();
-            Console.ResetColor()
-            Console.ForegroundColor <- model.color
-            Console.WriteLine(model.formatText);
-            let key = Console.ReadKey();
-            match key.Key with
-
-            | ConsoleKey.UpArrow ->
-                ChangePosition.Up |> ChangePosition |> KeyButton |> dispatch
-
-            | ConsoleKey.DownArrow ->
-                ChangePosition.Down |> ChangePosition |> KeyButton |> dispatch
-
-            | ConsoleKey.D1 -> ConsoleColor.Black |> ChangeColor |> EnterText |> dispatch
-            | ConsoleKey.D2 -> ConsoleColor.Red |> ChangeColor |> EnterText |> dispatch
-            | ConsoleKey.D3 -> ConsoleColor.Blue |> ChangeColor |> EnterText |> dispatch
-            | ConsoleKey.D4 -> ConsoleColor.Green |> ChangeColor |> EnterText |> dispatch
-            | ConsoleKey.D5 -> ConsoleColor.Cyan |> ChangeColor |> EnterText |> dispatch
-
-            | ConsoleKey.P -> Author.Push |> ChangeAuthor |> EnterText |> dispatch
-            | ConsoleKey.L -> Author.Ler |> ChangeAuthor |> EnterText |> dispatch
-            | ConsoleKey.E -> Author.Esenin |> ChangeAuthor |> EnterText |> dispatch
-            | ConsoleKey.B -> Author.Block |> ChangeAuthor |> EnterText |> dispatch
-
-            | _ -> failwith ""
 
 let getlines (str: string) startY endY =
         let lines = str.Split [| '\n' |]
@@ -86,83 +71,97 @@ let getlines (str: string) startY endY =
                 |> String.concat ("\n")
 
 
-let reverseUpdate (msg:ReverseMsg) (model:RevertModel) =
-    match msg with
-    |KeyButton button ->
-        
 
+type ButtonAction =
+    | ConsoleColor of ConsoleColor
+    | Author of Author
+    | ChangePosition of ChangePosition
 
 let readUpdate (msg: ReadMsg) (model: SnapshotInfo) =
     match msg with
-     | KeyButton button ->
-             let colorChangedButton = match button with
-                                         | ConsoleKey.D1 -> Some ConsoleColor.Black
-                                         | ConsoleKey.D2 -> Some ConsoleColor.Red
-                                         | ConsoleKey.D3 -> Some ConsoleColor.Blue
-                                         | ConsoleKey.D4 -> Some ConsoleColor.Green
-                                         | ConsoleKey.D5 -> Some ConsoleColor.Cyan
-                                         | _ -> None
+    | KeyButton button ->
 
-             match colorChangedButton with
-             | Some s -> { model with color = s },[]
-             | None ->
-                 let authorChangedButton = match button with
-                                             | ConsoleKey.P -> Some Author.Push
-                                             | ConsoleKey.B -> Some Author.Block
-                                             | ConsoleKey.E -> Some Author.Esenin
-                                             | ConsoleKey.L -> Some Author.Ler
-                                             | _ -> None
+            let (|>>) o1 o2 =
+               match o1 with
+               | Some s -> Some s
+               | None -> o2
 
-                 match authorChangedButton with
-                 | Some s ->
-                     let (Stix text) = seed.[s];
+            let q =
+                    Option.bind (ConsoleColor >> Some)
+                        (match button with
+                        | ConsoleKey.D1 -> Some ConsoleColor.Black
+                        | ConsoleKey.D2 -> Some ConsoleColor.Red
+                        | ConsoleKey.D3 -> Some ConsoleColor.Blue
+                        | ConsoleKey.D4 -> Some ConsoleColor.Green
+                        | ConsoleKey.D5 -> Some ConsoleColor.Cyan
+                        | _ -> None)
+
+
+            let q1 =
+                    Option.bind (Author >> Some)
+                     (match button with
+                     | ConsoleKey.P -> Some Author.Push
+                     | ConsoleKey.B -> Some Author.Block
+                     | ConsoleKey.E -> Some Author.Esenin
+                     | ConsoleKey.L -> Some Author.Ler
+                     | _ -> None)
+
+            let q3 =
+                    Option.bind (ChangePosition >> Some)
+                     (match button with
+                     | ConsoleKey.DownArrow -> Some ChangePosition.Down
+                     | ConsoleKey.UpArrow -> Some ChangePosition.Up
+                     | _ -> None)
+
+            match (q |>> q1 |>> q3) with
+            | Some s ->
+                match s with
+                | ConsoleColor color ->
+                    { model with color = color }, Cmd.ofMsg EventStore
+                | Author author ->
+                     let (Stix text) = seed.[author];
                      let formatText = getlines text 0 2
-                     {text = text; yPosition = 0; color = model.color; formatText = formatText}, Cmd.ofMsg EventStore
-                 | None -> model, []
+                     { text = text; yPosition = 0; color = model.color; formatText = formatText }, Cmd.ofMsg EventStore
+                | ChangePosition change ->
+                     match change with
+                     | Up ->
+                         if model.yPosition = 0
+                          then model, []
+                          else
+                          let formatText = getlines model.text (model.yPosition - 1) (model.yPosition + 1)
+                          { model with yPosition = model.yPosition - 1; formatText = formatText }, Cmd.ofMsg EventStore
+                     |Down ->
+                          if model.yPosition = (model.text.Split [| '\n' |]).Length - 1
+                           then model, []
+                           else
+                              let formatText = getlines model.text (model.yPosition + 1) (model.yPosition + 3)
+                              { model with yPosition = model.yPosition + 1; formatText = formatText }, Cmd.ofMsg EventStore
+
+
+            | None -> failwith "error"
+
+
+
+let reverseUpdate (msg:ReverseMsg) (model:RevertModel) =
+    model,[]
+
+
+
 
 
 
 
 
 let update (msg: Msg) (model: Model) =
-
-    match msg with
-    | EnterText r ->
-        match r with
-         | ChangeAuthorAndColor(a, c) ->
-                 let (Stix text) = seed.[a];
-                 let formatText = getlines text 0 2
-                 { text = text; yPosition = 0; color = c; formatText = formatText }, Cmd.ofMsg EventStore
-
-         | ChangeColor c ->
-             { model with color = c }, Cmd.ofMsg EventStore
-
-         | ChangeAuthor a ->
-                 let (Stix text) = seed.[a];
-                 let formatText = getlines text 0 2
-                 { text = text; yPosition = 0; color = model.color; formatText = formatText }, Cmd.ofMsg EventStore
-
-    | KeyButton tabKey ->
-        match tabKey with
-        | ChangePosition changePosition ->
-            match changePosition with
-
-            | ChangePosition.Down ->
-                if model.yPosition = (model.text.Split [| '\n' |]).Length - 1
-                  then model, []
-                  else
-                      let formatText = getlines model.text (model.yPosition + 1) (model.yPosition + 3)
-                      { model with yPosition = model.yPosition + 1; formatText = formatText }, Cmd.ofMsg EventStore
-
-            | ChangePosition.Up ->
-                if model.yPosition = 0
-                then model, []
-                else
-                      let formatText = getlines model.text (model.yPosition - 1) (model.yPosition + 1)
-                      { model with yPosition = model.yPosition - 1; formatText = formatText }, Cmd.ofMsg EventStore
-
-        | Back ->
-            storage.[storage.Length - 1], Cmd.ofMsg EventStore
+    match (msg,model) with
+    |(ReadMsg readmsg,Read readmodel) ->
+        let (model,cmd) = readUpdate readmsg readmodel
+        
+    |(ReverseMsg reverseMsg, Reverse (a,b)) ->
+        reverseUpdate reverseMsg (Reverse (a,b))
+        
+        
+        
 
 
 
